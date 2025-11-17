27db751267dec2dacb3c0f7c1ce80b8315695bdf
nightbringer <1136956273@qq.com>
2025-11-17 18:53:55 +0800

    Add MediaTransport class for JPEG-based video streaming

---
+++ src/media/MediaTransport.cpp
@@ -0,0 +1,110 @@
+﻿#include "MediaTransport.h"
+
+#include <QBuffer>
+#include <QHostAddress>
+#include <QImage>
+#include <QPixmap>
+#include <QVBoxLayout>
+
+#include "MediaEngine.h"
+
+MediaTransport::MediaTransport(MediaEngine *engine, QObject *parent)
+    : QObject(parent)
+    , udpSendSocket(new QUdpSocket(this))
+    , udpRecvSocket(new QUdpSocket(this))
+    , sendTimer(new QTimer(this))
+    , remoteIp()
+    , localPort(0)
+    , remotePort(0)
+    , remoteVideoLabel(new QLabel)
+    , remoteVideoWidget(new QWidget)
+    , media(engine)
+{
+    sendTimer->setInterval(40);
+    connect(sendTimer, &QTimer::timeout, this, &MediaTransport::onSendTimer);
+
+    remoteVideoLabel->setAlignment(Qt::AlignCenter);
+    remoteVideoLabel->setMinimumSize(320, 240);
+
+    auto *layout = new QVBoxLayout(remoteVideoWidget);
+    layout->setContentsMargins(0, 0, 0, 0);
+    layout->addWidget(remoteVideoLabel);
+}
+
+bool MediaTransport::startTransport(quint16 localPortValue, const QString &remoteIpValue, quint16 remotePortValue)
+{
+    stopTransport();
+
+    localPort = localPortValue;
+    remoteIp = remoteIpValue;
+    remotePort = remotePortValue;
+
+    if (!udpRecvSocket->bind(QHostAddress::AnyIPv4, localPort)) {
+        return false;
+    }
+
+    connect(udpRecvSocket, &QUdpSocket::readyRead, this, &MediaTransport::onReadyRead);
+
+    sendTimer->start();
+
+    return true;
+}
+
+void MediaTransport::stopTransport()
+{
+    if (sendTimer->isActive()) {
+        sendTimer->stop();
+    }
+
+    if (udpRecvSocket->isOpen()) {
+        udpRecvSocket->close();
+    }
+
+    udpRecvSocket->disconnect(this);
+}
+
+QWidget *MediaTransport::getRemoteVideoWidget()
+{
+    return remoteVideoWidget;
+}
+
+void MediaTransport::onSendTimer()
+{
+    if (!media || remoteIp.isEmpty() || remotePort == 0) {
+        return;
+    }
+
+    const QImage frame = media->getCurrentFrame();
+    if (frame.isNull()) {
+        return;
+    }
+
+    QByteArray buffer;
+    QBuffer qBuffer(&buffer);
+    qBuffer.open(QIODevice::WriteOnly);
+    if (!frame.save(&qBuffer, "JPG")) {
+        return;
+    }
+
+    if (!buffer.isEmpty()) {
+        udpSendSocket->writeDatagram(buffer, QHostAddress(remoteIp), remotePort);
+    }
+}
+
+void MediaTransport::onReadyRead()
+{
+    while (udpRecvSocket->hasPendingDatagrams()) {
+        QByteArray datagram;
+        datagram.resize(int(udpRecvSocket->pendingDatagramSize()));
+        udpRecvSocket->readDatagram(datagram.data(), datagram.size());
+
+        QImage image;
+        image.loadFromData(datagram, "JPG");
+        if (!image.isNull()) {
+            remoteVideoLabel->setPixmap(QPixmap::fromImage(image).scaled(remoteVideoLabel->size(),
+                                                                         Qt::KeepAspectRatio,
+                                                                         Qt::SmoothTransformation));
+        }
+    }
+}
+
---
+++ src/media/MediaTransport.h
@@ -0,0 +1,44 @@
+﻿#ifndef MEDIATRANSPORT_H
+#define MEDIATRANSPORT_H
+
+#include <QObject>
+#include <QUdpSocket>
+#include <QTimer>
+#include <QString>
+#include <QLabel>
+
+class MediaEngine;
+
+class MediaTransport : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit MediaTransport(MediaEngine *engine, QObject *parent = nullptr);
+
+    bool startTransport(quint16 localPort, const QString &remoteIp, quint16 remotePort);
+    void stopTransport();
+
+    QWidget *getRemoteVideoWidget();
+
+private slots:
+    void onSendTimer();
+    void onReadyRead();
+
+private:
+    QUdpSocket *udpSendSocket;
+    QUdpSocket *udpRecvSocket;
+    QTimer *sendTimer;
+
+    QString remoteIp;
+    quint16 localPort;
+    quint16 remotePort;
+
+    QLabel *remoteVideoLabel;
+    QWidget *remoteVideoWidget;
+
+    MediaEngine *media;
+};
+
+#endif // MEDIATRANSPORT_H
+
